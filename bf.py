f98b5f22178cdea062b7409674635259b789be3f
Tysonzero <danielspencersmith@gmail.com>
2015-02-02 18:58:00 -0700

    Reorder left() and right() functions

--- bf.py
+++ bf.py
@@ -2,15 +2,15 @@ data = [0]
 i = 0
 
 
-def left():
-    global i
-    if not i:
-        raise IndexError('index can\'t be negative')
-    i -= 1
-
-
 def right():
     global i
     i += 1
     if i == len(data):
         data.append(0)
+
+
+def left():
+    global i
+    if not i:
+        raise IndexError('index can\'t be negative')
+    i -= 1
