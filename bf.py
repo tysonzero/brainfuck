fbeb67cddc3e85bd659a069d729d7f099584f74c
Tysonzero <danielspencersmith@gmail.com>
2015-02-02 18:52:34 -0700

    Create left() and right() functions

--- bf.py
+++ bf.py
@@ -1,2 +1,16 @@
 data = [0]
 i = 0
+
+
+def left():
+    global i
+    if not i:
+        raise IndexError('index can\'t be negative')
+    i -= 1
+
+
+def right():
+    global i
+    i += 1
+    if i == len(data):
+        data.append(0)
